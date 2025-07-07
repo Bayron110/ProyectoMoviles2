5433f6316001b9529953f248f1bf081865f07772
ITSQMET ESTUDIANTES <bayron124z@hotmail.com>
2025-07-07 10:18:30 -0500

    ConexionABase

---
+++ firebase/ConfigFire.tsx
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZ7_pdPyDTLx8arkxyoJ8PtLW6pl2Hipc",
+  authDomain: "app-proyecto-27d00.firebaseapp.com",
+  projectId: "app-proyecto-27d00",
+  storageBucket: "app-proyecto-27d00.firebasestorage.app",
+  messagingSenderId: "616749071610",
+  appId: "1:616749071610:web:ce3a24909c3e1c81761d43"
+};
+
+const app = initializeApp(firebaseConfig);
